d6d2a33c2d48ef98f65ce0c118372c2ac393543c
Zaha250 <koltunov1998@mail.ru>
2021-12-23 16:48:10 +1000

    Подключение socket.io

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
 import './App.css';
 
+const socket = io();
+
 const App:React.FC = () => {
+
+  useEffect((): void => {
+    socket.on("connect", () => {
+      console.log(socket.connected); 
+    });
+  }, []);
+
   return(
     <h1>1</h1>
   )
