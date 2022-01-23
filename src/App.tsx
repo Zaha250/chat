13716b14003e352afda096679335ef9253f8efa1
Александр Колтунов <alex@MacBook-Pro-Aleksandr.local>
2022-01-23 16:31:37 +1000

    Вывод id socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import { io } from 'socket.io-client';
 import './App.css';
 
 const socket = io();
 
 const App:React.FC = () => {
+  const [id, setId] = useState<string | number>('');
 
   useEffect((): void => {
     socket.on("connect", () => {
       console.log(socket.connected); 
+      setId(socket.id);
     });
   }, []);
 
   return(
-    <h1>1</h1>
+    <h1>{id}</h1>
   )
 }
 
